959934c1e242349131c986e7dd43ab0cc6a3d416
frozenndev <125629242+Tdrizz@users.noreply.github.com>
2024-03-05 22:59:08 +0000

    Tue, Mar 5, 2024, 1:59 PM -09:00

--- journal.py
+++ journal.py
@@ -1,5 +1,8 @@
 # Reads number of lines from each file and returns total number of lines and program written with current date in lineswritten.txt file.
 from datetime import date
+import textwrap
+
+# Gets current date and stores it in variable
 today = date.today()
 
 # Gets material covered from user
@@ -19,10 +22,10 @@ for _ in range(programs):
         total += len(filename.readlines())
 
 # Opens journal.txt and writes input into the file with current date stamp
-myWrap = textwrap
+myWrap = textwrap.TextWrapper(width = 100)
 with open("journal.txt", "a") as totalLines:
     totalLines.write(f"Today is {today}\n\n")
-    totalLines.write(totalLines.fill(f"Material Covered: {materialCovered}\n\n"))
+    totalLines.write(myWrap.fill(f"Material Covered: {materialCovered}\n\n"))
     totalLines.write("\n\n")
     totalLines.write(f"How I'm Feeling: {feelingDaily}\n\n")
     totalLines.write(f"Lines/Program(s) Written: {total} lines and {programs} program(s)\n\n")
